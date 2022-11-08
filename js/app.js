5c0f5150d0b9303f34240f09957ef41e60eb5ef2
Marco Medici <edmundo.marco10@gmail.com>
2022-11-07 22:50:04 -0300

    entrega-final

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open__menu");
+const closeMenuBtn = document.querySelector(".close__menu");
+
+function toggleMenu() {
+    menu.classList.toggle("menu__opened")
+}
+
+openMenuBtn.addEventListener("click", toggleMenu)
+closeMenuBtn.addEventListener("click", toggleMenu)
